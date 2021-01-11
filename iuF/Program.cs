08d446cb5cffd5dbcb93a4d5f2b9fff2298fd75e
LHOUTELLIER Maël <55429341+mlhoutel@users.noreply.github.com>
2021-01-11 01:19:56 +0100

    Initialisation

---
+++ iuF/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+using System.Windows.Forms;
+
+namespace iuF {
+    public partial class Program : Form {
+        private static void Main(string[] args) {
+            Reader sensor = new Reader();
+            sensor.Run();
+            
+        }
+    }
+}
+*/
